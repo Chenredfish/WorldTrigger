f2f5aa9d3c6c5b22c6c02091e145895106536ac7
redfish <chenredfish940309@gmail.com>
2024-07-01 13:28:33 +0800

    增加地圖和角色動畫

--- main.gd
+++ main.gd
@@ -4,7 +4,8 @@ extends Node2D
 @onready var game_state_machine = $GameStateMachine
 #UI
 @onready var ui_layer = %UILayer
-
+#地圖
+@export var TEST_MAP = preload("res://src/map/test_map.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +16,9 @@ func _ready():
 func _process(delta):
 	pass
 
+func load_map():
+	var current_map = TEST_MAP.instantiate()
+	self.add_child(current_map)
 
 func _on_ui_layer_btn_new_game_pressed():
 	game_state_machine.set_value('is_new_game', true)
