c2680aa6de6ebc86da390de0b8d7e748c2158d60
redfish <chenredfish940309@gmail.com>
2024-07-04 16:08:25 +0800

    新增敵人(僅限畫面)，武器的物件，角色的基礎數值和函數

--- src/actor/test_actor.gd
+++ src/actor/test_actor.gd
@@ -1,16 +1,37 @@
 extends Node2D
-class_name Test_actor
+class_name TestActor
 
 signal show_skill_btn_pressed
+signal actor_die
+
+@export var attack_amount:int ##攻擊力
+@export var behavior_amount:int ##行動段數
+@export var max_health:int ##最大生命值
+@export var avoid:int ##閃避機率(0~100)
+@export var trigger:TestTrigger = TestTrigger.new()
+
+var current_trigger:TestTrigger
+var current_health:int ##現在血量
+var behaviors = []   ##陣列不能限定類別，切記這裡一定要放行動Behavior!!!
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	current_health = max_health
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+	
+func take_behavior(behavior:Behavior):
+	pass
+	
+func take_damage():
+	pass
+	
+func die():
+	actor_die.emit()
 
 func _on_show_skill_btn_pressed():
 	pass
--- src/map/test_map.gd
+++ src/map/test_map.gd
@@ -4,18 +4,25 @@ class_name TestMap
 signal show_skill_btn_pressed
 
 const TEST_ACTOR = preload("res://src/actor/test_actor.tscn")
+const SANDBAG = preload("res://src/enemy/sandbag.tscn")
 
-@export var start_position:Vector2i = Vector2i(4,4)
+@export var actor_start_position:Vector2i = Vector2i(4,4)
+@export var enemy_start_position:Vector2i
 
-var actor:Test_actor
+var actor:TestActor
+var enemy:Sandbag
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	actor = TEST_ACTOR.instantiate()
-	actor.set_position(Vector2(map_to_local(start_position)))
+	
+	#新增玩家
+	actor = add_role(TEST_ACTOR, actor_start_position)
 	actor.show_skill_btn_pressed.connect(_on_show_skill_btn_pressed)
 	self.add_child(actor)
 	
+	#新增敵人
+	enemy = add_role(SANDBAG, enemy_start_position)
+	self.add_child(enemy)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,3 +34,10 @@ func get_actor_map_position():
 
 func _on_show_skill_btn_pressed():
 	show_skill_btn_pressed.emit()
+
+func add_role(preload_role, position:Vector2i):
+	var role:Node2D
+	role = preload_role.instantiate()
+	role.set_position(Vector2(map_to_local(position)))
+	
+	return role
